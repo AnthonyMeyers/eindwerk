f6d684e28f4674f986468c0d035ddd245c22ff8c
AnthonyMeyers <thony.meyers@gmail.com>
2022-06-17 20:05:13 +0200

    changed routering

--- hoofdapplicatie/front_end/tdl-app/src/modules/RouterHeader.jsx
+++ hoofdapplicatie/front_end/tdl-app/src/modules/RouterHeader.jsx
@@ -4,7 +4,7 @@ import AppHeaderAppointments from "./appointment_modules/App-Header-Appointments
 import AppHeaderTodos from "./todo_modules/App-header-todos";
 import PhonebookHeader from "./phonebook_modules/Phonebook_header";
 import IndexHeader from "./login_modules/IndexHeader";
-import ProfileHeader from "./profile_modules/profileheader";
+import ProfileHeader from "./profile_modules/ProfileHeader";
 
 export default function RouterHeader() {
   //Show these headers on these routes
