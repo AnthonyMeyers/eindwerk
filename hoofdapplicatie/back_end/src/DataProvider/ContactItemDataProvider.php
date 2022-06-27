c8628f7e2ec63ae0186ccf4f4b44a64d2f1a8c36
AnthonyMeyers <thony.meyers@gmail.com>
2022-06-27 16:07:31 +0200

    added dataprovider for contact class

---
+++ hoofdapplicatie/back_end/src/DataProvider/ContactItemDataProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataProvider;
+
+use ApiPlatform\Core\DataProvider\ItemDataProviderInterface;
+use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
+Use App\Entity\Contact;
+use App\Repository\ContactRepository;
+use ApiPlatform\Core\DataProvider\SerializerAwareDataProviderInterface;
+use ApiPlatform\Core\DataProvider\SerializerAwareDataProviderTrait;
+use mysql_xdevapi\Exception;
+
+final class ContactItemDataProvider implements ItemDataProviderInterface, RestrictedDataProviderInterface, SerializerAwareDataProviderInterface
+{
+
+    use SerializerAwareDataProviderTrait;
+    private $repository;
+
+    //Geeft de contactpersoon terug of een lege array
+    public function __construct(ContactRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
+    {
+        // TODO: Implement supports() method.
+        return Contact::class === $resourceClass;
+    }
+
+    public function getItem(string $resourceClass, $id, string $operationName = null, array $context = [])
+    {
+        // TODO: Implement getItem() method.
+        $data = $this->repository->findOneBy(['id' => $id]);
+
+        if($id === 0)
+        {
+            return [] ;
+        }
+
+        return $data;
+
+
+    }
+}
